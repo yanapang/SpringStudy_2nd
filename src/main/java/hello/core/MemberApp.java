539826b03f1b99ca1672c26357407a575e2bd861
yanapang <yanahwang@gmail.com>
2023-03-21 21:38:10 +0900

    [객체 지향 원리 적용]AppConfig Spring Annotation 적용. Appconfig 객체를 생성해 사용하지 않고, Spring Container에 등록된 Bean을 통해 메서드 호출 후 사용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig 안에있는 환경 설정 정보를 가지고 spring container에 생성해 저장해놓는다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //어떤 메서드를 가지고 올지 정함, 두번째 파라미터는 타입.
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
