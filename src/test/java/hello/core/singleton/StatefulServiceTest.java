8d90349396818511de93015832292312fa230b28
yanapang <yanahwang@gmail.com>
2023-03-28 00:30:13 +0900

    [싱글톤 컨테이너] 싱글톤 방식의 주의점. 문제 해결. 무상태로 변수를 변경->지역변수를 이용해 문제 해결. 실무에서도 가끔 터지는 문제!!! 주의할것. -공유필드 조심. 스프링빈은 항상 무상태여야함.

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,13 +17,13 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A사용자 1만원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB: ㅠtkdydwk 2만원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA : 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("userAPrice = " + userAPrice);
         //싱글톤으로 인한 오류. price가 덮어씌워져 2만원이 나옴.
     }
 
