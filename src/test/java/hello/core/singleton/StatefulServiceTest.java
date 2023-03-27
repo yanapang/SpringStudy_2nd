eaa0abdc707483db820018b86b7a8be956601947
yanapang <yanahwang@gmail.com>
2023-03-28 00:25:16 +0900

    [싱글톤 컨테이너] 싱글톤 방식의 주의점. 문제 제시.

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A사용자 1만원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: ㅠtkdydwk 2만원 주문
+        statefulService2.order("userB", 20000);
+
+        //ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+        //싱글톤으로 인한 오류. price가 덮어씌워져 2만원이 나옴.
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
