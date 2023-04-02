dd4dd001e61a36620da69cf1b60d629ba8351cde
yanapang <yanahwang@gmail.com>
2023-04-02 21:39:43 +0900

    [컴포넌트 스캔] AppConfig @ComponentScan에서 탐색 패키지 지정가능. (@SpringBootApplication 프로젝트 시작 루트로 설정하는것 관례; componentScan 설정 포함되어있음.) 컴포넌트 스캔 기본 대상: @Component, Controller, Service, Repository,Configuration.

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member", //패키지 탐색 시작위치 지정. (속도 향상 가능). 여러개 지정가능. {"hello.core", "hello.service"}
+        //basePackageClasses = AutoAppConfig.class, //패키지 탐색 지정 클래스부터 시작.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
 }
