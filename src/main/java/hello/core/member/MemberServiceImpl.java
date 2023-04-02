0b264b7161d10cc58c4676caae7a679cc50e32ec
yanapang <yanahwang@gmail.com>
2023-04-02 21:21:02 +0900

    [컴포넌트 스캔] 컴포넌트 스캔, 의존관계 자동주입. @Configuration, @ComponentScan을 사용하면 더이상 @Bean을 등록하지 않아도 됨! AutoAppConfig파일 참고. 대신, 사용할 클래스들에 @Component 헤더 작성해주기. 의존관계 주입은 @Autowired로 사용. (MemberServiceImpl)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)와 동일한 역할을 해준다. 의존관계 주입.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
