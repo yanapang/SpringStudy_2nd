0c00945371128668c50584b0d6c2b078cabc8612
yanapang <yanahwang@gmail.com>
2023-04-04 21:34:12 +0900

    Revert "[의존관계 자동 주입] 조회 빈이 2개 이상일때 (문제)"
    This reverts commit 272fbe9a852ce1379a9503cd0318767a931df486.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,15 +15,11 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     /*
-    * @Qualifier 사용해 DiscountPolicy 구현체 지정
-    * 만약 @Qualifier값을 못찾으면 동명의 스프링빈을 추가로 찾는다.
-    * But, @Qualifier는 @Qualifier를 찾을때만 사용하는 것이 좋음.
-    * 해당 Qualifier 없을 경우 -> Bean 검색 -> 그도 없을 경우 NoSuchBeanDefinitionException 반환.
-    * */
+    * @Autowired 매칭*/
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+        this.discountPolicy = rateDiscountPolicy;
     }
 
     @Override
