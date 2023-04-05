c3cc9238e7ad9ad5130016f9ef2339506821bb80
yanapang <yanahwang@gmail.com>
2023-04-05 14:11:25 +0900

    [빈 스코프] 1. 스코프와 Provider. Object Provider를 사용해 싱글톤, 프로토타입 함께 사용할 경우 생기는 문제 해결.

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,39 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * web 스코프 중, request scope 지정.
+ * HTTP 요청 하나당 하나 생성.
+ */
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
